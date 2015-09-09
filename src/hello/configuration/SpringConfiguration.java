aa33d9908e6f4f448fa349c9ed15756ef6cb0ebe
Ricardo Kendy Horigome <ricardohorigome@gmail.com>
2015-09-09 20:13:29 -0300

    checkin inicial

---
+++ src/hello/configuration/SpringConfiguration.java
@@ -0,0 +1,11 @@
+package hello.configuration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "hello")
+public class SpringConfiguration {
+	
+}
---
+++ src/hello/configuration/SpringServletInitializer.java
@@ -0,0 +1,23 @@
+package hello.configuration;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+ 
+public class SpringServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+ 
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[] { SpringConfiguration.class };
+    }
+  
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return null;
+    }
+  
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] { "/" };
+    }
+ 
+}
+	 
